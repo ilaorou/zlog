74a86ca1fa48556195fb219eb780f2b314521500
tong <455656198@qq.com>
2022-12-07 10:45:32 +0800

    调整

--- zlog.go
+++ zlog.go
@@ -10,36 +10,46 @@ import (
 	"time"
 )
 
+const (
+	_createFileErrMsg = "Cannot create logFile:"
+	_hasZLoggerErrMsg = "has init ZLogger."
+)
+
 var levelMap = map[string]zapcore.Level{
-	"debug":  zapcore.DebugLevel,
-	"info":   zapcore.InfoLevel,
-	"warn":   zapcore.WarnLevel,
-	"error":  zapcore.ErrorLevel,
-	"dpanic": zapcore.DPanicLevel,
-	"panic":  zapcore.PanicLevel,
-	"fatal":  zapcore.FatalLevel,
+	"debug": zapcore.DebugLevel,
+	"info":  zapcore.InfoLevel,
+	"warn":  zapcore.WarnLevel,
+	"error": zapcore.ErrorLevel,
+	"panic": zapcore.PanicLevel,
+	"fatal": zapcore.FatalLevel,
 }
 
-//type sugaredLogger struct {
-//	*zap.SugaredLogger
-//	init bool
-//}
+type ZLogger struct {
+	*zap.SugaredLogger
+	init bool
+}
 
-var sugaredLogger *zap.SugaredLogger
+var zLogger = &ZLogger{}
 
 //func init() {
-//sugaredLogger = NewLogger("log.log", "debug", true, 10, 10, 10)
+//	logger, _ := zap.NewProduction()
+//	defer logger.Sync() // flushes buffer, if any
+//	zLogger.SugaredLogger = logger.Sugar()
 //}
 
-func NewLogger(fileName, level string, mod bool, maxSize, maxBackups, maxAge int) {
-	//if sugaredLogger.init {
-	//	sugaredLogger.Error("has init sugaredLogger")
-	//	return sugaredLogger
-	//}
+func (z *ZLogger) Close() {
+	//刷新缓冲区
+	zLogger.SugaredLogger.Sync()
+}
+
+func NewLogger(fileName, level string, dev bool, maxSize, maxBackups, maxAge int) *ZLogger {
+	if zLogger.init {
+		zLogger.Error(_hasZLoggerErrMsg)
+		return zLogger
+	}
 	zapLevel, ok := levelMap[level]
 	if !ok { //默认info等级输出
 		zapLevel = zapcore.InfoLevel
-		level = "Hello"
 	}
 	//fileName := filepath.Join(logDir, logFile)
 	_, err := os.Stat(fileName)
@@ -48,87 +58,105 @@ func NewLogger(fileName, level string, mod bool, maxSize, maxBackups, maxAge int
 		if err == nil {
 			file.Close()
 		} else {
-			fmt.Println("Cannot create logFile:", fileName)
+			fmt.Println(_createFileErrMsg, fileName)
 			os.Exit(1)
 		}
-
 	}
-	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	//encoderConfig := zap.NewProductionEncoderConfig()
+	//encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 	//encoderConfig.EncodeLevel = func(l zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
 	//	enc.AppendString(cases.Title(language.English).String(l.String())) //自定义level文字输出格式
 	//}
-	encoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
-		enc.AppendString(t.Format("2006-01-02 15:04:05"))
-	}
-	encoder := zapcore.NewConsoleEncoder(encoderConfig) //返回文本
-	writeSyncer := zapcore.AddSync(&lumberjack.Logger{
-		Filename:   fileName,   // 日志文件路径
-		MaxSize:    maxSize,    // 每个日志文件保存的最大尺寸 单位：M
-		MaxBackups: maxBackups, // 日志文件最多保存多少个备份
-		MaxAge:     maxAge,     // 文件最多保存多少天
-		Compress:   true,       // 是否压缩
-	})
+	//encoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	//	enc.AppendString(t.Format("2006-01-02 15:04:05"))
+	//}
 
-	if zapLevel == zapcore.DebugLevel { //开发者模式，多路输出
-		writeSyncer = zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), writeSyncer)
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:       "ts",
+		LevelKey:      "level",
+		NameKey:       "logger",
+		CallerKey:     "caller",
+		FunctionKey:   zapcore.OmitKey,
+		MessageKey:    "msg",
+		StacktraceKey: "stacktrace",
+		LineEnding:    zapcore.DefaultLineEnding,
+		EncodeLevel:   zapcore.CapitalLevelEncoder,
+		EncodeTime: func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+			enc.AppendString(t.Format("2006-01-02 15:04:05"))
+		},
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
-	core := zapcore.NewCore(encoder, writeSyncer, zapLevel)
+	//输出格式
+	encoder := zapcore.NewConsoleEncoder(encoderConfig) //plain-text 字符串
+	//encoder := zapcore.NewJSONEncoder(encoderConfig)  //json 文本
 
+	var core zapcore.Core
 	var log *zap.Logger
-	if mod { //调试时，输出具体行号
-		log = zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+	if dev { //开发者模式
+		core = zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zapLevel)
+		//writeSyncer = zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), writeSyncer)//多路输出
+		log = zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1)) //输出行号
 	} else {
+		//控制输出的位置
+		writeSyncer := zapcore.AddSync(&lumberjack.Logger{
+			Filename:   fileName,   // 日志文件路径
+			MaxSize:    maxSize,    // 每个日志文件保存的最大尺寸 单位：M
+			MaxBackups: maxBackups, // 日志文件最多保存多少个备份
+			MaxAge:     maxAge,     // 文件最多保存多少天
+			Compress:   true,       // 是否压缩
+		})
+		core = zapcore.NewCore(encoder, writeSyncer, zapLevel)
 		log = zap.New(core)
 	}
-	sugaredLogger = log.Sugar()
-	//sugaredLogger.init = true
-	//return sugaredLogger
+	zLogger.SugaredLogger = log.Sugar()
+	zLogger.init = true
+	return zLogger
 }
 
 func Json(arg interface{}) {
 	body, _ := json.Marshal(arg)
-	sugaredLogger.Debug(string(body))
+	zLogger.SugaredLogger.Debug(string(body))
 }
 func Debug(args ...interface{}) {
-	sugaredLogger.Debug(args...)
+	zLogger.SugaredLogger.Debug(args...)
 }
 func Debugf(template string, args ...interface{}) {
-	sugaredLogger.Debugf(template, args...)
+	zLogger.SugaredLogger.Debugf(template, args...)
 }
 func Info(args ...interface{}) {
-	sugaredLogger.Info(args...)
+	zLogger.SugaredLogger.Info(args...)
 }
 func Infof(template string, args ...interface{}) {
-	sugaredLogger.Infof(template, args...)
+	zLogger.SugaredLogger.Infof(template, args...)
 }
 func Warn(args ...interface{}) {
-	sugaredLogger.Warn(args...)
+	zLogger.SugaredLogger.Warn(args...)
 }
 func Warnf(template string, args ...interface{}) {
-	sugaredLogger.Warnf(template, args...)
+	zLogger.SugaredLogger.Warnf(template, args...)
 }
 func Error(args ...interface{}) {
-	sugaredLogger.Error(args...)
+	zLogger.SugaredLogger.Error(args...)
 }
 func Errorf(template string, args ...interface{}) {
-	sugaredLogger.Errorf(template, args...)
+	zLogger.SugaredLogger.Errorf(template, args...)
 }
 func DPanic(args ...interface{}) {
-	sugaredLogger.DPanic(args...)
+	zLogger.SugaredLogger.DPanic(args...)
 }
 func DPanicf(template string, args ...interface{}) {
-	sugaredLogger.DPanicf(template, args...)
+	zLogger.SugaredLogger.DPanicf(template, args...)
 }
 func Panic(args ...interface{}) {
-	sugaredLogger.Panic(args...)
+	zLogger.SugaredLogger.Panic(args...)
 }
 func Panicf(template string, args ...interface{}) {
-	sugaredLogger.Panicf(template, args...)
+	zLogger.SugaredLogger.Panicf(template, args...)
 }
 func Fatal(args ...interface{}) {
-	sugaredLogger.Fatal(args...)
+	zLogger.SugaredLogger.Fatal(args...)
 }
 func Fatalf(template string, args ...interface{}) {
-	sugaredLogger.Fatalf(template, args...)
+	zLogger.SugaredLogger.Fatalf(template, args...)
 }
